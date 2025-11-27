3527aa546031769dc94e8c426a8f0a9bb46d93da
undefinedSahed <sahedrahmanshakil@gmail.com>
2025-11-28 03:35:33 +0600

    add: privacy policy and terms and conditions page

--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -1,9 +1,14 @@
+import PrivacyTermsContent from '@/components/shared/privacy-terms-content'
+import PrivacyTermsTop from '@/components/shared/privacy-terms-top'
 import React from 'react'
+import policyData from "@/data/privacy-terms-data.json";
+
 
 export default function page() {
     return (
-        <div>
-            Terms and Conditions Page
+        <div className="bg-[url('/backgrounds/home-bg.jpg')]">
+            <PrivacyTermsTop title="Terms & Conditions" description="Your privacy is important to us at Untitled. We respect your privacy regarding any information we may collect from you across our website." />
+            <PrivacyTermsContent data={policyData.terms_and_conditions} />
         </div>
     )
 }
